869f19b398ace0f155fb5cf62d24518cd070fbea
Stroong-Bee <lintangsyahdewo26@gmail.com>
2026-03-02 11:18:04 +0700

    update

--- src/app/Signal/page.tsx
+++ src/app/Signal/page.tsx
@@ -8,8 +8,6 @@ export default function SignalPage() {
     <div className="min-h-screen bg-[#050505] text-white flex flex-col">
       {/* NAVBAR */}
       <Navbar
-        // activeTab="signal"
-        // setActiveTab={() => {}}
         onSync={() => {}}
         isLoading={false}
       />
